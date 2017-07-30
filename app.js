cff1bbee116f81f3928b5caae65a55e97be7698f
KingCastorius <malizakzorz@gmail.com>
2017-07-30 15:28:49 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+let lifesMeaning = 42;
+
+let sayName() = false;
+
+console.log('What is the meaning of life?')
+
+function sayName() {
+  if(lifesMeaning) {
+    return 'correct';
+  }else {
+    return 'incorrect'
+  }
+}
+
+console.log(sayName());
+
+// build a quiz app
+// 1. in the  console i need to ask a question
+// 2. Pass an answer to my question function
+// 3.if the answer is correct, console log "correct", else console log incorrect
